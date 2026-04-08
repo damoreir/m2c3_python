db2ebe01f0aa6191971c86fad2032b8995c96a24
Daniel <damoreir1@gmail.com>
2026-04-08 18:34:03 +0200

    python commit

---
+++ Course_Checkpoint_3.py
@@ -0,0 +1,70 @@
+#___________Exercice 1___________
+
+sentence = 'The quick brown fox jumped over the lazy dog' #String
+
+number = 120 #Number
+
+frutas = [10,20,30] #List
+
+boolean = (1 > 0) #True (Boolean)
+
+
+
+#___________Exercice 2___________
+
+primera_palavra = sentence [0:3]
+# print(primera_palavra)
+
+
+
+#___________Exercice 3___________
+
+primero_elemento = frutas [0]
+# print(primero_elemento)
+
+
+
+#___________Exercice 4___________
+
+new_number = number +  10
+# print(new_number)
+
+
+
+#___________Exercice 5___________
+
+ultimo_elemento = frutas [-1]
+# print(ultimo_elemento)
+
+
+
+#___________Exercice 6___________
+
+names = 'harry,alex,susie,jared,gail,conner'
+# print(names.split(','))
+
+
+
+#___________Exercice 7___________
+
+first_word = sentence [0:3]
+
+new_sentence = first_word.upper() + sentence[3:]
+
+# print(new_sentence)
+
+
+
+#___________Exercice 8___________
+
+str = f'''
+En el año pasado tuve la oportunidad de ganar {number} euros en un sorteo
+'''.strip()
+
+# print(str)
+
+
+
+#___________Exercice 9___________
+
+print('hello world')
